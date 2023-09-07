2639d4a0f8dce6a79233d569201a8892bbd585f6
Joel Norrman <joel-norrman@hotmail.com>
2023-09-07 23:57:27 +0200

    uppgifter1

--- src/com/JOEL/demo/UppgiftPart2.java
+++ src/com/JOEL/demo/UppgiftPart2.java
@@ -2,7 +2,7 @@ package com.JOEL.demo;
 
 public class UppgiftPart2 {
     public static void main(String[] args) {
-        int[] score = new int[]{0, 1, 4, 6, 12};
+        int[] score = new int[]{1, 2, 4, 5, 8};
         System.out.println(score[0]);
     }
 }
--- src/com/JOEL/demo/UppgiftPart7.java
+++ src/com/JOEL/demo/UppgiftPart7.java
@@ -1,4 +1,14 @@
 package com.JOEL.demo;
 
 public class UppgiftPart7 {
+    public static void main(String[] args) {
+        int[] score = new int[]{1, 2, 4, 5, 8};
+        for (int value : score) {
+            if (value % 2 == 0) {
+                System.out.println(value + " Is even");
+            } else {
+                System.out.println(value + " Is odd");
+            }
+        }
+    }
 }
